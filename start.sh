fc648e2e7989c3137be2b399db2d6d517be3dbb7
Yolan <yolanother@gmail.com>
2023-06-25 23:05:42 -0700

    Add server args

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ echo "Current working directory: $(pwd)"
 
 source venv/bin/activate
 pip install -r requirements.txt
-venv/bin/python3 main.py
+venv/bin/python3 main.py --listen 0.0.0.0 --port 8180 --cpu
